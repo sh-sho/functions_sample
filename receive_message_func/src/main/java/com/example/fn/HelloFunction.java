0f1893ab7becb6593b577cb736f8b52799dab0cb
sh-sho <sho.obata.c5@gmail.com>
2023-12-18 08:10:46 +0000

    forEachを修正

--- receive_message_func/src/main/java/com/example/fn/HelloFunction.java
+++ receive_message_func/src/main/java/com/example/fn/HelloFunction.java
@@ -12,10 +12,7 @@ public class HelloFunction {
     private static Logger log = LoggerFactory.getLogger(HelloFunction.class);
 
     public String handleRequest(List<Input> input) {
-
-        for (Input putValue : input) {
-            log.info(new String(Base64.getDecoder().decode(putValue.value)));
-        }
+        input.forEach(s -> log.info(new String(Base64.getDecoder().decode(s.value))));
         String decodeStr = new String(Base64.getDecoder().decode(input.get(0).value));
         return decodeStr;
     }
